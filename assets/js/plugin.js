d6fd9b7e9029b54944815ec25ade37950d91c443
zubayerhimel <zhimel007@gmail.com>
2020-06-09 23:48:45 +0600

    added search input field

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,4 +1,12 @@
 $(function () {
   $(".sidenav").sidenav();
   $("select").formSelect();
+
+  // SEACH IN THE TABLE
+  $("#search").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#filter_info tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
 });
